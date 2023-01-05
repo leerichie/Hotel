f7c08858213fe773fb1819e01a1e0568e1a6da78
leerichie <leerichie@wp.pl>
2023-01-05 20:33:36 +0100

    first_Hotel

---
+++ src/main/java/com/example/hotel/response/ReservationResponse.java
@@ -0,0 +1,53 @@
+package response;
+
+import model.Links;
+
+public class ReservationResponse {
+
+    private Long id;
+    private Integer roomNumber;
+    private Integer price;
+    private Links link;
+
+    public ReservationResponse() {
+        super();
+    }
+
+    public ReservationResponse(Integer roomNumber, Integer price) {
+        super();
+        this.roomNumber = roomNumber;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Links getLink() {
+        return link;
+    }
+
+    public void setLink(Links link) {
+        this.link = link;
+    }
+}
---
+++ src/main/java/com/example/hotel/rest/ResourceConstants.java
@@ -0,0 +1,8 @@
+package rest;
+
+
+public class ResourceConstants {
+
+    public static final String ROOM_RESERVATION_V1 = "/room/reservation/v1";
+
+}
