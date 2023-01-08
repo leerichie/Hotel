47cccf3f549ea71ca9225f6199a39ad738dda7a7
leerichie <leerichie@wp.pl>
2023-01-08 14:04:20 +0100

    CRUD_and_Front-end

--- src/main/java/com/example/hotel/config/ApiConfig.java
+++ src/main/java/com/example/hotel/config/ApiConfig.java
@@ -5,11 +5,11 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ApiConfig {
 
-    @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
--- src/main/java/com/example/hotel/rest/ReservationResource.java
+++ src/main/java/com/example/hotel/rest/ReservationResource.java
@@ -33,4 +33,18 @@ public class ReservationResource {
         return new ResponseEntity<>(new ReservationResponse(), HttpStatus.CREATED);
     }
 
+    @RequestMapping(path = "", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ReservationResponse> updateReservation(
+            @RequestBody
+            ReservationRequest reservationRequest) {
+        return new ResponseEntity<>(new ReservationResponse(), HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/{reservationId}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> deleteReservation (
+            @PathVariable
+            long reservationId) {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 }
