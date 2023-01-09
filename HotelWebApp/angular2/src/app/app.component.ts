8338d323f9eef2173ec8fddb2ada6bfce02e49a9
leerichie <leerichie@wp.pl>
2023-01-09 18:21:49 +0100

    update

--- HotelWebApp/angular2/src/app/app.component.ts
+++ HotelWebApp/angular2/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular2';
+export class AppComponent implements OnInit {
+
+  public submitted: boolean;
+  roomsearch: FormGroup;
+
+  // title = 'A&A app working! :)';
+
+  ngOnInit() {
+    this.roomsearch = new FormGroup({
+      checkin: new FormControl(''),
+      checkout: new FormControl('')
+    });
+  }
+
+  onSubmit({value,valid}: { value: Roomsearch, valid: boolean }) {
+    console.log(value);
+  }
+}
+
+  export interface Roomsearch{
+    checkin: String;
+    checkout: String;
 }
--- HotelWebApp/angular2/src/app/app.module.ts
+++ HotelWebApp/angular2/src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from '@angular/common/http'
+import {HttpClient} from '@angular/common/http'
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
+    // AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
